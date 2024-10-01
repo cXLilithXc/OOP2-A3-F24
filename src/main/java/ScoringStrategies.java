66f5d1ce6ad2a73130dc5a4a41c5a46d33285de2
Flo234253 <florenceler3@gmail.com>
2024-10-01 14:00:01 -0400

    Changed where the files where

--- src/main/java/ScoringStrategies.java
+++
@@ -1,9 +0,0 @@
-import com.champlain.oop2assignment3.CardCollection;
-
-public class ScoringStrategies implements ScoringStrategy {
-
-    @Override
-    public int calculateScore(CardCollection pCards) {
-        return pCards.size();
-    }
-}
--- src/main/java/ScoringStrategy.java
+++
@@ -1,5 +0,0 @@
-import com.champlain.oop2assignment3.CardCollection;
-
-public interface ScoringStrategy {
-    int calculateScore(CardCollection pCards);
-}
